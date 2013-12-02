9812e681a98073fb0d15fbbb42cff88ce5fffd1c
gustavo <gustavo.santos0106@gmail.com>
2013-12-02 00:40:36 -0200

    clean up building script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 rm /home/gustavo/zips/*.zip
 /home/gustavo/kernel/build_cm.sh
 /home/gustavo/kernel/makezip.sh
-/home/gustavo/kernel/build_cm.twrp.sh
+make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/cm_initramfs.twrp.list"
 /home/gustavo/kernel/makezip.twrp.sh
-/home/gustavo/kernel/build_omni.sh
+make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/omni_initramfs.list"
 /home/gustavo/kernel/makezip_omni.sh
-/home/gustavo/kernel/build_omni.twrp.sh
+make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/omni_initramfs.twrp.list"
 /home/gustavo/kernel/makezip_omni.twrp.sh
 
