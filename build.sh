85a64c2c669b699eb5e4fd93ea7633716706a4f5
gustavo <gustavo.santos0106@gmail.com>
2013-12-03 17:44:11 -0200

    add slimkat support

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/omni_initramfs
 /home/gustavo/kernel/makezip_omni.sh
 make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/omni_initramfs.twrp.list"
 /home/gustavo/kernel/makezip_omni.twrp.sh
+make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/slimkat_initramfs.list"
+/home/gustavo/kernel/makezip_slimkat.sh
+make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/slimkat_initramfs.list"
+/home/gustavo/kernel/makezip_slimkat.twrp.sh
 
